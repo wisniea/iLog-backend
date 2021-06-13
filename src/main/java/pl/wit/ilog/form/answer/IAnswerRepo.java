4af08938fa7bbfd3439778fe64320b5e28129b1c
wisniea <masterflip9999@gmail.com>
2021-06-13 16:14:08 +0200

    remove objects

--- src/main/java/pl/wit/ilog/form/answer/IAnswerRepo.java
+++ src/main/java/pl/wit/ilog/form/answer/IAnswerRepo.java
@@ -1,7 +1,10 @@
 package pl.wit.ilog.form.answer;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import pl.wit.ilog.form.question.QuestionEntity;
+
+import java.util.stream.Stream;
 
 public interface IAnswerRepo extends JpaRepository<AnswerEntity, Long> {
-
+    Stream<AnswerEntity> findAllByQuestion(QuestionEntity question);
 }
--- src/main/java/pl/wit/ilog/form/web/AnswerController.java
+++ src/main/java/pl/wit/ilog/form/web/AnswerController.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import pl.wit.ilog.form.answer.AnswerEntity;
 import pl.wit.ilog.form.answer.AnswerRequest;
@@ -16,7 +17,9 @@ import pl.wit.ilog.internals.web.IMapper;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import java.net.URI;
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RestController
@@ -29,10 +32,29 @@ public class AnswerController {
 
     private final IMapper<AnswerEntity, AnswerResponse> mapper;
 
+    @Transactional
+    @GetMapping("/{formUuid}/questions/{questionId}/answers/{answerId}")
+    public AnswerResponse getAnswer(@Valid @PathVariable("formUuid") final UUID formUuid,
+                                  @Valid @PathVariable("questionId") final Long questionId,
+                                  @Valid @PathVariable("answerId") final Long answerId) {
+        return answerRepo.findById(answerId)
+                .map(mapper::map)
+                .orElseThrow(EntityNotFoundException::new);
+    }
 
+    @Transactional
+    @GetMapping("/{formUuid}/questions/{questionId}/answers")
+    public List<AnswerResponse> getAllAnswers(@Valid @PathVariable("formUuid") final UUID formUuid,
+                                              @Valid @PathVariable("questionId") final Long questionId) {
+        val question = questionRepo.findById(questionId)
+                .orElseThrow(EntityNotFoundException::new);
+        return answerRepo.findAllByQuestion(question)
+                .map(mapper::map)
+                .collect(Collectors.toList());
+    }
 
     @PreAuthorize("hasRole('USER')")
-    @PostMapping("{formUuid}/questions/{questionId}")
+    @PostMapping("/{formUuid}/questions/{questionId}")
     public ResponseEntity<AnswerResponse> create(@RequestBody @NotNull final AnswerRequest request,
                                  @Valid @PathVariable("formUuid") final UUID formUuid,
                                  @Valid @PathVariable("questionId") final Long questionId) throws Exception{
@@ -47,5 +69,17 @@ public class AnswerController {
         val response = mapper.map(answer);
         return ResponseEntity.created(new URI("/forms/" + formUuid + "/questions/"+ questionId+ "answers/" + response.getId().toString()))
                 .body(response);
+    }
+
+    @PreAuthorize("hasRole('USER')")
+    @DeleteMapping("/{formUuid}/questions/{questionId}/answers/{answerId}")
+    public void delete(@Valid @PathVariable("formUuid") final UUID formUuid,
+                       @Valid @PathVariable("questionId") final Long questionId,
+                       @Valid @PathVariable("questionId") final Long answerId){
+
+        val answer = answerRepo.findById(answerId)
+                .orElseThrow(EntityNotFoundException::new);
+
+        answerRepo.delete(answer);
     }
 }
