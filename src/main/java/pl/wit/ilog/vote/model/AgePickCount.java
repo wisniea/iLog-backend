492a11ea2b26f46f56b33c190a3d9d518d34862a
Dand3Z <62472767+Dand3Z@users.noreply.github.com>
2021-06-13 16:25:10 +0200

    More Custom Queries

---
+++ src/main/java/pl/wit/ilog/vote/model/AgePickCount.java
@@ -0,0 +1,14 @@
+package pl.wit.ilog.vote.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class AgePickCount {
+
+    private Integer age;
+    private Long count;
+}
--- src/main/java/pl/wit/ilog/vote/model/IVoteRepo.java
+++ src/main/java/pl/wit/ilog/vote/model/IVoteRepo.java
@@ -10,9 +10,32 @@ import java.util.List;
 @Repository
 public interface IVoteRepo extends JpaRepository<VoteEntity, Long> {
 
-    @Query("SELECT NEW pl.wit.ilog.vote.model.AnswerPickCount(p.answerId, count(p.id)) FROM PickEntity p WHERE p.answerId = :answerId GROUP BY p.answerId")
-    AnswerPickCount countByPickEntityIdGroupByAnswerId(@Param("answerId") Long answerId);
+    // how many times a given answer was selected
+    @Query("SELECT NEW pl.wit.ilog.vote.model.AnswerPickCount(p.answerId, count(p.id)) FROM PickEntity p WHERE p.answerId = :answerId")
+    AnswerPickCount answerIdPicksAmount(@Param("answerId") Long answerId);
 
     @Query("SELECT NEW pl.wit.ilog.vote.model.AnswerPickCount(p.answerId, count(p.id)) FROM PickEntity p GROUP BY p.answerId")
-    List<AnswerPickCount> countByPickEntityIdGroupByAnswerId();
+    List<AnswerPickCount> allAnswerIdsPicksAmount();
+
+    // how many people of each age voted
+    @Query("SELECT NEW pl.wit.ilog.vote.model.AgePickCount(v.age, count(v.age)) FROM VoteEntity v WHERE v.age = :age")
+    AgePickCount allAgePicksAmount(@Param("age") Integer age);
+
+    @Query("SELECT NEW pl.wit.ilog.vote.model.AgePickCount(v.age, count(v.age)) FROM VoteEntity v GROUP BY v.age")
+    List<AgePickCount> allAgesPicksAmount();
+
+    // how many people of each sex voted
+    @Query("SELECT NEW pl.wit.ilog.vote.model.SexPickCount(v.sex, count(v.sex)) FROM VoteEntity v WHERE v.sex = :sex")
+    SexPickCount allSexPicksAmount(@Param("sex") SexEnum sex);
+
+    @Query("SELECT NEW pl.wit.ilog.vote.model.SexPickCount(v.sex, count(v.sex)) FROM VoteEntity v GROUP BY v.sex")
+    List<SexPickCount> allSexesPicksAmount();
+
+    // how many people of each sex chose a particular answer
+
+
+
+    // =============== TEXT ANSWERS ===============
+
+
 }
